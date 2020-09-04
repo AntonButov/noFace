9b3a0a9fd5fd272228784f4e49e56ea04fa9eed6
Anton Butov <butov6101@gmail.com>
2020-09-04 12:40:29 +0300

    Добавил FCM.

--- app/src/main/java/pro/butovanton/noface/MyFirebaseInstanceIdService.kt
+++ app/src/main/java/pro/butovanton/noface/MyFirebaseInstanceIdService.kt
@@ -1,5 +1,20 @@
 package pro.butovanton.noface
 
+import android.util.Log
+import com.google.firebase.iid.FirebaseInstanceId
 import com.google.firebase.iid.FirebaseInstanceIdService
+import pro.butovanton.noface.di.App.Companion.TAG
 
-class MyFirebaseInstanceIdService : FirebaseInstanceIdService()
+
+class MyFirebaseInstanceIdService : FirebaseInstanceIdService() {
+    override fun onTokenRefresh() {
+        // Get updated InstanceID token.
+        val refreshedToken = FirebaseInstanceId.getInstance().token
+        Log.d(TAG, "Refreshed token: $refreshedToken")
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+ //       sendRegistrationToServer(refreshedToken)
+    }
+}
---
+++ app/src/main/java/pro/butovanton/noface/MyFirebaseMessagingService.kt
@@ -0,0 +1,52 @@
+package pro.butovanton.noface
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.media.RingtoneManager
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import pro.butovanton.noface.di.App.Companion.TAG
+
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    private var mNotificationManager: NotificationManager? = null
+    var notifyId = 0
+    var notifyCount = 0
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+        Log.d(TAG, "onMessageReceived")
+        mNotificationManager =
+            getSystemService(NOTIFICATION_SERVICE) as NotificationManager // Создаем экземпляр менеджера уведомлений
+        val builder: NotificationCompat.Builder
+        val CHANNEL_ID = "2"
+        val title = if (remoteMessage.notification!!.title != null) remoteMessage.notification!!
+            .title else "NoFace"
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID, "NoFace",
+                NotificationManager.IMPORTANCE_MIN
+            )
+            channel.description = "Admin"
+            mNotificationManager!!.createNotificationChannel(channel)
+        }
+        builder = NotificationCompat.Builder(this, CHANNEL_ID)
+        builder.setChannelId(CHANNEL_ID)
+            .setSmallIcon(R.drawable.chat_bubble_outline_24px)
+            .setColor(resources.getColor(R.color.whiteGray))
+            .setNumber(notifyCount++)
+            .setContentText(remoteMessage.notification!!.body)
+            .setContentTitle(title)
+            .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+            .setAutoCancel(true)
+        mNotificationManager!!.notify(notifyId++, builder.build())
+    }
+
+    override fun onNewToken(s: String) {
+        super.onNewToken(s)
+    }
+}
